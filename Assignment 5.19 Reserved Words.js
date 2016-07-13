e2b7b5dbf066988c2736e238152f5b725b216584
jnguyen66 <jvnguyen66@gmail.com>
2016-07-12 20:31:13 -0700

    Add files via upload

---
+++ Assignment 5.19 Reserved Words.js
@@ -0,0 +1,9 @@
+alert("Hello");
+var alert = 2;
+alert("World");
+ 
+/*
+This script does not run because alert is a html/javascript reserved word. 
+Therefore it cannot be intialized as a variable and assigned to 2.
+Because 
+*/
---
+++ Assignment 5.27 Movie Constructor.js
@@ -0,0 +1,26 @@
+
+function makeMovie(name,rating, directorList, producersList, studio, date) {
+
+        this.title = name
+
+        this.mpaa =rating
+
+        this.directors=directorList
+
+        this.producers=producersList
+
+        this.studio-studio
+
+        this.release=date
+
+                }
+
+
+   var name = "Justin's Big Film";
+   var rating =9.0;
+   var directors = ["J.J. Abrams" , "Spielberg", "Quentin Taratino"];
+   var producers=["George Lucas", "Peter Jackson"];
+   var studio ="Sony";
+   var release="June 14, 1990";
+
+   makeMovie(name, rating, directors, producers, studio, release);
